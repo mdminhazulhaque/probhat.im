514c6f82afa51659b78e22f7fb9ee1ba98f05ab5
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2016-07-10 03:36:32 +0600

    Compressed JS

--- js/jquery.ime.inputmethods.js
+++ js/jquery.ime.inputmethods.js
@@ -1,18 +1,1 @@
-( function ( $ ) {
-	'use strict';
-
-	$.extend( $.ime.sources, {
-		'bn-probhat': {
-			name: 'প্রভাত',
-			source: 'probhat.im/js/bn-probhat.js'
-		}
-	} );
-
-	$.extend( $.ime.languages, {
-		bn: {
-			autonym: 'বাংলা',
-			inputmethods: [ 'bn-probhat' ]
-		}
-	} );
-
-}( jQuery ) );
+!function(e){"use strict";e.extend(e.ime.sources,{"bn-probhat":{name:"প্রভাত",source:"probhat.im/js/bn-probhat.js"}}),e.extend(e.ime.languages,{bn:{autonym:"বাংলা",inputmethods:["bn-probhat"]}})}(jQuery);
