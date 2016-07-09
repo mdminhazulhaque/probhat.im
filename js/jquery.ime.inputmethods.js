47c22184f19e0a70bff61609799bf8772aa8c1f0
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2016-07-09 09:54:18 +0600

    First stable release

---
+++ js/jquery.ime.inputmethods.js
@@ -0,0 +1,18 @@
+( function ( $ ) {
+	'use strict';
+
+	$.extend( $.ime.sources, {
+		'bn-probhat': {
+			name: 'প্রভাত',
+			source: 'js/bn-probhat.js'
+		}
+	} );
+
+	$.extend( $.ime.languages, {
+		bn: {
+			autonym: 'বাংলা',
+			inputmethods: [ 'bn-probhat' ]
+		}
+	} );
+
+}( jQuery ) );
