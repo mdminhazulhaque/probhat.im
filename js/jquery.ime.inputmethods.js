b5d224e62385b81481d7e790f53957837bf0e901
Md. Minhazul Haque <minhazul-haque@users.noreply.github.com>
2017-07-29 13:58:40 +0600

    Update jquery.ime.inputmethods.js

--- js/jquery.ime.inputmethods.js
+++ js/jquery.ime.inputmethods.js
@@ -1,1 +1,1 @@
-!function(e){"use strict";e.extend(e.ime.sources,{"bn-probhat":{name:"প্রভাত",source:"js/bn-probhat.js"}}),e.extend(e.ime.languages,{bn:{autonym:"বাংলা",inputmethods:["bn-probhat"]}})}(jQuery);
+!function(e){"use strict";e.extend(e.ime.sources,{"bn-probhat":{name:"প্রভাত",source:"//js/bn-probhat.js"}}),e.extend(e.ime.languages,{bn:{autonym:"বাংলা",inputmethods:["bn-probhat"]}})}(jQuery);
