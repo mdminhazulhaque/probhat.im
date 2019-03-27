f8ed491d6dcc556202e214ee3813e9751721b33c
Md. Minhazul Haque <mdminhazulhaque@gmail.com>
2019-03-27 17:34:33 +0800

    Remove old files as resources will be fetched from CDN

--- js/jquery.ime.inputmethods.js
+++
@@ -1,1 +0,0 @@
-!function(e){"use strict";e.extend(e.ime.sources,{"bn-probhat":{name:"প্রভাত",source:"probhat.im/js/bn-probhat.js"}}),e.extend(e.ime.languages,{bn:{autonym:"বাংলা",inputmethods:["bn-probhat"]}})}(jQuery);
